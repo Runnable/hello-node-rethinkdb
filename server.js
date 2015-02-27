10a531cb8dde88aa228d2604f9d486626c57f766
anandkumarpatel <anand@runnable.com>
2015-02-27 13:33:49 -0800

    Update server.js

--- server.js
+++ server.js
@@ -2,12 +2,8 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res){
-    res.send('Anton is heredas');
+    res.send('anand is heredas');
 });
 
 app.listen(80);
 
-
-// anton is good at coding
-
-// yash is pacing around
