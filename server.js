9940c2fa0dc2b98a59d0f603a8c77ebcfe8a500b
anandkumarpatel <anand@runnable.com>
2015-02-27 13:38:45 -0800

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res){
-    res.send('anand is heredas');
+    res.send('anand ETIMEDOUT');
 });
 
 app.listen(80);
