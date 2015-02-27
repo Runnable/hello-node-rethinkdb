1ee01d9e31f531ea5765cba6228e6855215053ac
anandkumarpatel <anand@runnable.com>
2015-02-27 14:50:03 -0800

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
-var express = require('express');
-var app = express();
+var http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World');
+}).listen(process.env.PORT || 80);
 
-app.get('/', function(req, res){
-    res.send('commit5');
-});
-
-app.listen(80);
+console.log('Server running at http://127.0.0.1:80/')
