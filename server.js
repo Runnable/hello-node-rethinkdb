d252f8d85e4550b6a09cb8a33cccccdbfd354d02
Anton Podviaznikov <podviaznikov@gmail.com>
2015-05-14 18:00:36 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('12');
+  res.end('13');
 }).listen(process.env.PORT || 80);
 
 console.log('Server running at http://127.0.0.1:80/')
